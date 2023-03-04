c5c38ab10653a9eefcbf947197c68a6ab86fd9da
arisygdc <arisycso@gmail.com>
2023-03-05 06:51:15 +0700

    rename folder

--- infra/web_server.sh
+++ infra/web_server.sh
@@ -2,7 +2,7 @@
 
 podman run -d --name web-server \
     -p 8080:80 \
-    -v /home/arisygdc/Projects/gopath/src/ikmv2/infra/site-available:/etc/apache2/sites-available \
-    -v /home/arisygdc/Projects/gopath/src/ikmv2/infra/site-enabled:/etc/apache2/sites-enabled \
+    -v /home/arisygdc/Projects/gopath/src/ikmv2/infra/apache/site-available:/etc/apache2/sites-available \
+    -v /home/arisygdc/Projects/gopath/src/ikmv2/infra/apache/site-enabled:/etc/apache2/sites-enabled \
     -v /home/arisygdc/Projects/gopath/src/ikmv2/frontend:/var/www/html \
     docker.io/php:8.2-apache
