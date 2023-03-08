8df439a29646c3f6a1fa461f32f9404b305c3ad8
arisygdc <arisycso@gmail.com>
2023-03-08 09:01:36 +0700

    backen test

---
+++ backend/backend_test.go
@@ -0,0 +1,151 @@
+package backend
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"testing"
+
+	"github.com/ikmv2/backend/api"
+	"github.com/ikmv2/backend/config"
+	"github.com/ikmv2/backend/pkg/repository"
+	"github.com/stretchr/testify/assert"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+var repo repository.Repository
+var conn *mongo.Client
+var category [3]string
+var sumPerCategory [3]int
+var DummyData []primitive.ObjectID
+var initData int = 40
+
+func TestMain(m *testing.M) {
+
+	for i := range category {
+		category[i] = repository.RandName(true)
+	}
+
+	cfg := config.MongoConfig{
+		MongoDriver: "mongodb",
+		User:        "user",
+		Password:    "secret",
+		Address:     "127.0.0.1",
+		DbName:      "ikm-project",
+	}
+
+	log.Println("connecting to database")
+	db, err := repository.ConnectDatabase(cfg)
+	if err != nil {
+		log.Fatal()
+	}
+	log.Println("database connected")
+
+	log.Println("fill database arg dan set dummy data")
+	conn = db.Client()
+	repo = repository.NewRepository(db)
+	seedData()
+
+	log.Println("Running tests!")
+
+	exitVal := m.Run()
+
+	db.Collection("catalog").DeleteMany(context.TODO(), bson.D{})
+	conn.Disconnect(context.TODO())
+	log.Println("wiping data")
+
+	log.Println("exiting tests!")
+	os.Exit(exitVal)
+}
+
+func seedData() {
+	insertParam := make([]repository.DocCatalog, initData)
+	for i := range insertParam {
+		Category := repository.RandInt(0, 3)
+		sumPerCategory[Category]++
+		insertParam[i] = repository.DocCatalog{
+			Name:        repository.RandName(),
+			Category:    category[Category],
+			Description: repository.RandString(15),
+			Owner:       primitive.NewObjectID().Hex(),
+			Foto:        primitive.NewObjectID().Hex() + ".jpg",
+		}
+	}
+
+	for i := range insertParam {
+		insrd, err := repo.Insert(context.TODO(), insertParam[i])
+		if err != nil {
+			continue
+		}
+
+		id := insrd.(primitive.ObjectID)
+
+		primID := id
+		log.Print(primID)
+		DummyData = append(DummyData, primID)
+	}
+}
+
+func TestGetPagination(t *testing.T) {
+	defer func() {
+		if r := recover(); r != nil {
+			fmt.Println("Recovered in f", r)
+		}
+	}()
+	maxPage := len(DummyData) / api.MaxProductPerPage
+	if maxPage > 0 {
+		maxPage += 1
+	}
+
+	log.Println("load server")
+	node := api.NewEndpoint(repo)
+	server := node.Server()
+	var last_id = ""
+	for i := 1; i <= maxPage; i++ {
+		path := fmt.Sprintf("/catalog/%d", i)
+		body, err := EncodeID(last_id)
+		if !assert.NoError(t, err) {
+			break
+		}
+
+		tp := TestSetParam{Name: "page", Value: fmt.Sprint(i)}
+		c, rec := CreateRequestContext(server, path, body, tp)
+
+		t.Log("request address: ", c.Path())
+		err = node.GetCatalog(c)
+		if !assert.NoError(t, err) {
+			t.Log(err)
+			break
+		}
+
+		if !assert.Equal(t, http.StatusOK, rec.Code) {
+			t.Log(rec.Body.String())
+			break
+		}
+
+		var js = struct {
+			Catalog []repository.DocCatalog `json:"catalog"`
+		}{}
+
+		err = json.Unmarshal(rec.Body.Bytes(), &js)
+
+		if !assert.NoError(t, err) {
+			break
+		}
+
+		ctlg := js.Catalog
+		t.Log("get response length: ", len(ctlg))
+
+		last_id = verifyOutput(t, i, maxPage, ctlg, DummyData)
+	}
+}
+
+// TODO
+// Pagination per category
+// rolling id pagination
+// Find error case
--- go.mod
+++ go.mod
@@ -4,10 +4,12 @@ go 1.20
 
 require (
 	github.com/labstack/echo/v4 v4.10.2
+	github.com/stretchr/testify v1.8.2
 	go.mongodb.org/mongo-driver v1.11.2
 )
 
 require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/golang/snappy v0.0.1 // indirect
 	github.com/klauspost/compress v1.13.6 // indirect
 	github.com/labstack/gommon v0.4.0 // indirect
@@ -15,6 +17,7 @@ require (
 	github.com/mattn/go-isatty v0.0.17 // indirect
 	github.com/montanaflynn/stats v0.0.0-20171201202039-1bf9dbcd8cbe // indirect
 	github.com/pkg/errors v0.9.1 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasttemplate v1.2.2 // indirect
 	github.com/xdg-go/pbkdf2 v1.0.0 // indirect
@@ -26,4 +29,5 @@ require (
 	golang.org/x/sync v0.0.0-20210220032951-036812b2e83c // indirect
 	golang.org/x/sys v0.5.0 // indirect
 	golang.org/x/text v0.7.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
