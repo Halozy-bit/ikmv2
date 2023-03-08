5c6fcba92dece5267fb92bb071a8186899345830
arisygdc <arisycso@gmail.com>
2023-03-08 08:58:31 +0700

    refactor code

--- backend/pkg/repository/repository.go
+++ backend/pkg/repository/repository.go
@@ -10,12 +10,12 @@ import (
 )
 
 type Repository struct {
-	coll Colletion
+	Colletion
 }
 
 func NewRepository(db *mongo.Database) Repository {
 	cimp := collectionImp{db: db}
-	return Repository{coll: &cimp}
+	return Repository{Colletion: &cimp}
 }
 
 // OPTIONAL TODO
@@ -26,14 +26,14 @@ func NewRepository(db *mongo.Database) Repository {
 //		{Key: "field2", Value: 1},
 //	})
 
-func (r *Repository) catalogQueryFirstPage(ctx context.Context, contentLimit int64, filter bson.D) (*mongo.Cursor, error) {
+func (r *Repository) catalogQueryFirstLine(ctx context.Context, contentLimit int64, filter bson.D) (*mongo.Cursor, error) {
 	opt := &options.FindOptions{}
 	opt.SetLimit(contentLimit)
 
-	return r.coll.Catalog().Find(ctx, filter, opt)
+	return r.Catalog().Find(ctx, filter, opt)
 }
-func (r *Repository) CatalogFirstPage(ctx context.Context, contentLimit int64) ([]DocCatalog, error) {
-	curr, err := r.catalogQueryFirstPage(ctx, contentLimit, bson.D{})
+func (r *Repository) CatalogFirstLine(ctx context.Context, contentLimit int64) ([]DocCatalog, error) {
+	curr, err := r.catalogQueryFirstLine(ctx, contentLimit, bson.D{})
 	if err != nil {
 		return nil, err
 	}
@@ -44,7 +44,7 @@ func (r *Repository) CatalogFirstPage(ctx context.Context, contentLimit int64) (
 func (r *Repository) CatalogFirstPageWithCategory(ctx context.Context, contentLimit int64, category string) ([]DocCatalog, error) {
 	filter := bson.D{{Key: "kategori", Value: category}}
 
-	curr, err := r.catalogQueryFirstPage(ctx, contentLimit, filter)
+	curr, err := r.catalogQueryFirstLine(ctx, contentLimit, filter)
 	if err != nil {
 		return nil, err
 	}
@@ -58,7 +58,7 @@ func (r *Repository) catalogFromId(ctx context.Context, id interface{}, contentL
 	opt.SetLimit(contentLimit)
 	filter := bson.D{{Key: "_id", Value: id}}
 
-	return r.coll.Catalog().Find(ctx, filter, opt)
+	return r.Catalog().Find(ctx, filter, opt)
 }
 
 // search the catalog with the underlying query selector
@@ -71,14 +71,39 @@ func (r *Repository) catalogIdSelector(ctx context.Context, id bson.D, contentLi
 	return DecodeCatalogCursor(ctx, curr)
 }
 
-func (r *Repository) CataloGtId(ctx context.Context, id primitive.ObjectID, contentLimit int64) ([]DocCatalog, error) {
+// get catalog from greater than @id
+func (r *Repository) CatalogGtId(ctx context.Context, id primitive.ObjectID, contentLimit int64) ([]DocCatalog, error) {
 	gtID := bson.D{{Key: "$gt", Value: id}}
 	return r.catalogIdSelector(ctx, gtID, contentLimit)
 }
 
+// get catalog from greater or equal @id
 func (r *Repository) CatalogGteId(ctx context.Context, id primitive.ObjectID, contentLimit int64) ([]DocCatalog, error) {
 	gtID := bson.D{{Key: "$gte", Value: id}}
 	return r.catalogIdSelector(ctx, gtID, contentLimit)
+}
+
+func (r *Repository) Insert(ctx context.Context, in DocCatalog) (interface{}, error) {
+	doc := DocumentFromModel(in)
+	res, err := r.Catalog().InsertOne(ctx, doc)
+	return res.InsertedID, err
+}
+
+func (r *Repository) CatalogFindOne(ctx context.Context, filter bson.D) (DocCatalog, error) {
+	var doc DocCatalog
+	err := r.Catalog().FindOne(context.TODO(), filter).Decode(&doc)
+
+	return doc, err
+}
+
+func (r *Repository) FirstItem() (DocCatalog, error) {
+	var doc DocCatalog
+	findOptions := options.FindOne()
+	// Sort by id field descending
+	findOptions.SetSort(bson.D{{Key: "_id", Value: 1}})
+	err := r.Catalog().FindOne(context.TODO(), bson.D{}).Decode(&doc)
+
+	return doc, err
 }
 
 func (r *Repository) LastItem() (DocCatalog, error) {
@@ -87,19 +112,12 @@ func (r *Repository) LastItem() (DocCatalog, error) {
 	findOptions.SetSort(bson.D{{Key: "_id", Value: -1}})
 
 	var doc DocCatalog
-	err := r.coll.Catalog().FindOne(context.TODO(), bson.D{}, findOptions).Decode(&doc)
-
-	return doc, err
-}
-
-func (r *Repository) FirstItem() (DocCatalog, error) {
-	var doc DocCatalog
-	err := r.coll.Catalog().FindOne(context.TODO(), bson.D{}).Decode(&doc)
+	err := r.Catalog().FindOne(context.TODO(), bson.D{}, findOptions).Decode(&doc)
 
 	return doc, err
 }
 
 func (r *Repository) CountCatalog(ctx context.Context) (int64, error) {
 	filter := bson.D{}
-	return r.coll.Catalog().CountDocuments(ctx, filter)
+	return r.Catalog().CountDocuments(ctx, filter)
 }
