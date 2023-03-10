207cb1b27eb28f6e218c0dcf46d7f210d557087f
arisygdc <arisycso@gmail.com>
2023-03-10 17:54:49 +0700

    fix nil category

--- backend/pkg/cache/cache.go
+++ backend/pkg/cache/cache.go
@@ -7,20 +7,25 @@ import (
 	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
-var Pagination *pagination
+var Pagination = new(pagination)
 
 func init() {
-	Pagination = new(pagination)
-	Pagination.mux = new(sync.RWMutex)
+	Pagination.Register()
 }
 
 var ErrStoreSameID = fmt.Errorf("cache store same id")
+var ErrStoreEmptyIDS = fmt.Errorf("cache store empty ids")
 
 // Immutable
 type pagination struct {
 	page     []primitive.ObjectID
 	category map[string][]primitive.ObjectID
 	mux      *sync.RWMutex
+}
+
+func (p *pagination) Register() {
+	p.mux = new(sync.RWMutex)
+	p.category = make(map[string][]primitive.ObjectID)
 }
 
 func (p *pagination) Leng() int {
@@ -30,6 +35,7 @@ func (p *pagination) Leng() int {
 	return leng
 }
 
+// starts from page 1
 func (p *pagination) Page(idx int) primitive.ObjectID {
 	p.mux.RLock()
 	defer p.mux.RUnlock()
@@ -54,6 +60,10 @@ func (p *pagination) CategoryPage(category string, idx int) primitive.ObjectID {
 func (p *pagination) StoreCategory(category string, ids []primitive.ObjectID) error {
 	p.mux.Lock()
 	defer p.mux.Unlock()
+	if len(ids) < 1 {
+		return ErrStoreEmptyIDS
+	}
+
 	if len(p.category[category]) > 0 {
 		if p.category[category][0] == ids[0] {
 			return ErrStoreSameID
@@ -67,11 +77,16 @@ func (p *pagination) StoreCategory(category string, ids []primitive.ObjectID) er
 func (p *pagination) StorePage(ids []primitive.ObjectID) error {
 	p.mux.Lock()
 	defer p.mux.Unlock()
+	if len(ids) < 1 {
+		return ErrStoreEmptyIDS
+	}
+
 	if len(p.page) > 0 {
 		if p.page[0] == ids[0] {
 			return ErrStoreSameID
 		}
 	}
+
 	p.page = ids
 	return nil
 }
