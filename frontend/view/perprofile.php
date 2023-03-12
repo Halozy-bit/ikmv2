573277aee443df4c19d2ae74de13dce4eaac5804
jamalludin jamalludin <jamalludin1999z@gmail.com>
2023-03-12 20:58:37 +0700

    add perprofile

--- frontend/view/perprofile.php
+++ frontend/view/perprofile.php
@@ -2,7 +2,7 @@
 
     require_once(getcwd() . '/header.html');
 
-    require_once(getcwd() . "/product/hero.html");
+    require_once(getcwd() . "/perprofile/hero.html");
 
     require_once(getcwd() . './footer.html');
 
