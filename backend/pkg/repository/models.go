796e7aca37000e6cd1bdad6c6d45d0f1c483a9cf
arisygdc <arisycso@gmail.com>
2023-03-15 10:03:33 +0700

    plan to change model

--- backend/pkg/repository/models.go
+++ backend/pkg/repository/models.go
@@ -4,7 +4,14 @@ import (
 	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
-const CategoryField = "kategori"
+const (
+	CategoryField  = "kategori"
+	ThumbnailField = "thumbnail"
+)
+
+// TODO
+// Foto change to array focuse on per product
+// Thumbnail for displaying catalog
 
 type DocCatalog struct {
 	Id          primitive.ObjectID `bson:"_id" insert:"0"`
@@ -12,5 +19,23 @@ type DocCatalog struct {
 	Category    string             `bson:"kategori"`
 	Description string             `bson:"deskripsi"`
 	Owner       string             `bson:"owner"`
+	Thumbnail   string             `bson:"thumbnail,omitempty"`
 	Foto        string             `bson:"foto"`
+}
+
+type CatalogDisplay struct {
+	Id        primitive.ObjectID `bson:"_id" json:"id"`
+	Name      string             `bson:"nama" json:"nama"`
+	Category  string             `bson:"kategori" json:"kategori"`
+	Owner     string             `bson:"owner" json:"owner"`
+	Thumbnail string             `bson:"thumbnail" json:"thumbnail"`
+}
+
+type Product struct {
+	Id          primitive.ObjectID `bson:"_id" json:"id"`
+	Name        string             `bson:"nama" json:"nama"`
+	Category    string             `bson:"kategori" json:"kategori"`
+	Description string             `bson:"deskripsi" json:"deskripsi"`
+	Owner       string             `bson:"owner" json:"owner"`
+	Foto        string             `bson:"foto" json:"foto"`
 }
