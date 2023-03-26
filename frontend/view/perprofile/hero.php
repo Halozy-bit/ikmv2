a97737e65cd348028a4bdac98ac1681b03d105c8
jamalludin jamalludin <jamalludin1999z@gmail.com>
2023-03-26 19:44:00 +0700

    edit hero image

--- frontend/view/perprofile/hero.php
+++ frontend/view/perprofile/hero.php
@@ -16,7 +16,7 @@
         <div class="container">
             <div class="d-lg-flex flex-row  justify-content-around">
                 <div class="d-flex col-lg-6 justify-content-center">
-                    <img class="img-fluid" style="height:320px; border-radius:5%;" src="<?php echo $val['foto_owner']['f_wawancara'] ?>" alt="">
+                    <img class="img-fluid" style="border-radius:5%;" src="<?php echo $val['foto_owner']['f_wawancara'] ?>" alt="">
                 </div>
                 <div class="my-3 col-lg-6 d-flex flex-column justify-content-center">
                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-tag-fill" viewBox="0 0 16 16">
@@ -31,13 +31,13 @@
             </div>
             <div class="d-lg-flex flex-row-reverse justify-content-around">
                 <div class="d-flex col-lg-6 justify-content-center">
-                    <img class="img-fluid" style="height:320px; border-radius:5%;" src="<?php echo $val['foto_owner']['f_produksi'] ?>" alt="">
+                    <img class="img-fluid" style="border-radius:5%;" src="<?php echo $val['foto_owner']['f_produksi'] ?>" alt="">
                 </div>
                 <div class="py-3 d-flex col-lg-6 flex-column justify-content-center">
                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-tag-fill" viewBox="0 0 16 16">
                         <path d="M2 1a1 1 0 0 0-1 1v4.586a1 1 0 0 0 .293.707l7 7a1 1 0 0 0 1.414 0l4.586-4.586a1 1 0 0 0 0-1.414l-7-7A1 1 0 0 0 6.586 1H2zm4 3.5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0z"/>
                       </svg>
-                    <h3>Legalitas</h3>
+                    <h3>Informasi</h3>
                     <p><?php 
                     $mp = $val['marketplace'];
                     $sm = $val['social_media'];
